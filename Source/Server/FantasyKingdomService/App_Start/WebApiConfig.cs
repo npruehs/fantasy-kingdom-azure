7ae4b8492c89fbe01df3e5a34d9cc2473b24ad89
Nick Prühs <dev@npruehs.de>
2015-08-06 19:12:58 +0200

    ADDED: Project - Downloaded project template files.

---
+++ Source/Server/FantasyKingdomService/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using FantasyKingdomService.DataObjects;
+using FantasyKingdomService.Models;
+
+namespace FantasyKingdomService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            new MobileAppConfiguration()
+                .UseDefaultConfiguration()
+                .ApplyTo(config);
+
+            Database.SetInitializer(new FantasyKingdomInitializer());
+        }
+    }
+
+    public class FantasyKingdomInitializer : ClearDatabaseSchemaIfModelChanges<FantasyKingdomContext>
+    {
+        protected override void Seed(FantasyKingdomContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
---
+++ Source/Server/FantasyKingdomService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FantasyKingdomService.WebApiApplication" Language="C#" %>
